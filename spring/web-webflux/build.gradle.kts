4b4a2ac59ae4cd5f418a4182488fd413083b4fda
haejin <megachj@gmail.com>
2021-05-01 20:51:31 +0900

    .

--- spring/web-webflux/build.gradle.kts
+++ spring/web-webflux/build.gradle.kts
@@ -1,20 +1,6 @@
-//plugins {
-//    id("org.asciidoctor.convert") version "2.4.0" // spring-rest-docs
-//}
-
 val springDocOpenApiUiVersion: String by ext
 
-//tasks {
-//    asciidoctor {
-//        dependsOn(test)
-//    }
-//    bootJar {
-//        dependsOn(asciidoctor)
-//    }
-//}
-
 dependencies {
-    implementation(project(":spring:library:jpa"))
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
